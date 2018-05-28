0ec01aa42c6937dbf98744d757480d459e9bbdfe
samuel grahame <samuel@intelogy.com.au>
2018-05-28 12:51:12 +1000

    Added Response convert response to dynamic

---
+++ SolvupSDK/Response.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SolvupSDK
+{
+    public class Response
+    {        
+        public static dynamic GetResponse(string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+                return null;
+
+            XDocument doc = XDocument.Parse(source);
+            string jsonText = JsonConvert.SerializeXNode(doc);
+            return JsonConvert.DeserializeObject<ExpandoObject>(jsonText);
+        }
+    }
+
+}
